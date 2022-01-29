5534936cdbfd7d1dad18521d5b0d18f8edf52f0b
SivachenkoEgor <georgegraygithub@gmail.com>
2022-01-30 05:07:42 +0700

    Add second exercise

---
+++ 3D adventure/Assets/Scripts/OogaBoogaMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OogaBoogaMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var direction = new Vector3(horizontal, 0f, vertical);
+
+        transform.position += direction;
+    }
+}
